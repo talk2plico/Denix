2cd1bbb09f61a3220907225f6930d35df1e92c50
talk2plico <talk2plico@gmail.com>
2024-11-19 11:13:34 +0100

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css'; // Optional: Import styles for the Footer
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; November 1, 2024 Denix. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
